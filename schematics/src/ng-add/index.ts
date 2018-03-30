b50cd1ffb8ad74a6987eb7dd212523a216edd9fd
ManfredSteyer <manfred.steyer@gmx.net>
2018-03-30 22:00:13 +0200

    update

--- schematics/src/ng-add/index.ts
+++ schematics/src/ng-add/index.ts
@@ -5,7 +5,6 @@ import { addDeclarationToAppModule } from './add-declaration-to-module.rule';
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function ngAdd(options: any): Rule {
-  
   return (tree: Tree, _context: SchematicContext) => {
     const appModule = '/src/app/app.module.ts';
     let rule = branchAndMerge(addDeclarationToAppModule(appModule));
